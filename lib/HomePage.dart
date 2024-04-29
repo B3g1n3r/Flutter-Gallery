012b0c38bd62b2e41f20766ebb20754b1bfc71a6
Eswaran <111951792+B3g1n3r@users.noreply.github.com>
2024-04-29 10:43:26 +0530

    Update HomePage.dart

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -7,7 +7,7 @@ import 'dart:io';
 
 class MyHomePage extends StatelessWidget {
   final String title;
-  const MyHomePage({super.key, required this.title});
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -19,19 +19,44 @@ class MyHomePage extends StatelessWidget {
       ),
       body: GridView.builder(
         gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 3, crossAxisSpacing: 8.0, mainAxisExtent: 150.0),
+          crossAxisCount: 3,
+          crossAxisSpacing: 8.0,
+          mainAxisExtent: 150.0,
+        ),
         itemCount: imageProvider.images.length,
         itemBuilder: (context, index) {
+          final imageFile = imageProvider.images[index];
           return GestureDetector(
             onTap: () {
               Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) => FullScreenImageScreen(
-                          imageFile: imageProvider.images[index],
-                          index: index)));
+                context,
+                MaterialPageRoute(
+                  builder: (context) => FullScreenImageScreen(
+                    imageFile: imageFile,
+                    index: index,
+                  ),
+                ),
+              );
             },
-            child: Image.file(imageProvider.images[index], fit: BoxFit.cover),
+            child: Container(
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.grey),
+                borderRadius: BorderRadius.circular(8),
+              ),
+              clipBehavior: Clip.antiAlias,
+              child: imageFile.existsSync()
+                  ? Image.file(
+                      imageFile,
+                      fit: BoxFit
+                          .contain, // Changed from BoxFit.cover to BoxFit.contain
+                      errorBuilder: (BuildContext context, Object exception,
+                          StackTrace? stackTrace) {
+                        // Display a placeholder or error message
+                        return Placeholder();
+                      },
+                    )
+                  : Placeholder(), // Display a placeholder if image file doesn't exist
+            ),
           );
         },
       ),
